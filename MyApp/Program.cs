279fd049a9f57f6355162aeb112e477202f69488
Etheryen <lukasz.ciwoniuk@gmail.com>
2026-03-11 12:57:05 +0100

    Improve output clarity

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿var nums = new List<int> { 1, 2, 3 };
 foreach (var num in nums)
-    Console.WriteLine(num);
+    Console.Write(num);
+Console.WriteLine();
 
-Console.WriteLine("Hello, check these numbers out!");
+Console.WriteLine("\nHello, check these numbers out!");
